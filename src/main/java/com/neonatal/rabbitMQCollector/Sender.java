cad8e959c7a62b4c321a6940ac853edca7c3c90a
Pravin Prabhakaran <pravin.prabha123248@gmail.com>
2023-02-06 01:13:01 +0000

    Added host name to message
    Name will be entered on nodes and tracked by server

--- src/main/java/com/neonatal/rabbitMQCollector/Sender.java
+++ src/main/java/com/neonatal/rabbitMQCollector/Sender.java
@@ -2,8 +2,10 @@ package com.neonatal.rabbitMQCollector;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Service;
+import java.util.Random;
 
 //Sender class responsible for sending data according to the communication schedule
 //Sender represents a node in the system that collects data.
@@ -14,8 +16,15 @@ public class Sender {
     @Autowired
     private RabbitTemplate rabbitTemplate;
 
+    //Understandable identifier
+    @Value("${rabbitmq.name}")
+    private String name;
+
+    //Combined with name to uniquely identify Sender
+    private int ID = Math.abs(new Random().nextInt());
+
     public void sendMessage(String message) {
-        System.out.println("Sent to data queue");
+        System.out.println("Sent to data queue by " + name + " " + ID);
         rabbitTemplate.convertAndSend("data", message);
     }
 
