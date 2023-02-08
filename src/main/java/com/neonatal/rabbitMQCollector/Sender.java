6d9040e0c71095b50a77e34d32efe27c5a0b74b6
Pravin Prabhakaran <pravin.prabha123248@gmail.com>
2023-02-08 23:14:08 +0000

    Update Sender.java

--- src/main/java/com/neonatal/rabbitMQCollector/Sender.java
+++ src/main/java/com/neonatal/rabbitMQCollector/Sender.java
@@ -6,7 +6,6 @@ import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Profile;
-import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 
 import java.io.*;
